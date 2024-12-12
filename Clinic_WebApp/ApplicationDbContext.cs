746e0f64b3d223730ceed94c02a7700b826f96c2
SalehM909 <saleh.m.alshabibi@gmail.com>
2024-12-12 14:33:13 +0400

    Program

--- Clinic_WebApp/ApplicationDbContext.cs
+++ Clinic_WebApp/ApplicationDbContext.cs
@@ -1,6 +1,38 @@
-﻿namespace Clinic_WebApp
+﻿using Clinic_WebApp.Models;
+using Clinic_WebApp.Models;
+using Clinic_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinic_WebApp
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Clinic> Clinics { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Booking>()
+                .HasKey(b => new { b.PatientID, b.ClinicID });
+
+            modelBuilder.Entity<Booking>()
+                .HasOne(b => b.Patient)
+                .WithMany(p => p.Bookings)
+                .HasForeignKey(b => b.PatientID);
+
+            modelBuilder.Entity<Booking>()
+                .HasOne(b => b.Clinic)
+                .WithMany(c => c.Bookings)
+                .HasForeignKey(b => b.ClinicID);
+        }
     }
+
 }
--- Clinic_WebApp/Program.cs
+++ Clinic_WebApp/Program.cs
@@ -1,15 +1,37 @@
 
+using Clinic_WebApp.Repositories;
+using Clinic_WebApp.Services;
+using Clinic_WebApp.Repositories;
+using Clinic_WebApp.Services;
+using Clinic_WebApp;
+using Microsoft.EntityFrameworkCore;
+
 namespace Clinic_WebApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+
             var builder = WebApplication.CreateBuilder(args);
+
+            // Connection String
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             // Add services to the container.
 
+            builder.Services.AddScoped<IPatientRepo, PatientRepo>();
+            builder.Services.AddScoped<IClinicRepo, ClinicRepo>();
+            builder.Services.AddScoped<IBookingRepo, BookingRepo>();
+
+            builder.Services.AddScoped<IPatientService, PatientService>();
+            builder.Services.AddScoped<IClinicService, ClinicService>();
+            builder.Services.AddScoped<IBookingService, BookingService>();
+
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -32,5 +54,6 @@ namespace Clinic_WebApp
 
             app.Run();
         }
+
     }
 }
